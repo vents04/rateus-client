2ed768472e91d966220b84fa14f919ac876c3ad8
vents04 <vencidim04@gmail.com>
2021-04-02 22:57:21 +0300

    responsive home and apply pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ApplyComponent } from './pages/apply/apply.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'apply', component: ApplyComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
